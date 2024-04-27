4f3a98b083b561796dd3fa96321bfa50feea8bd8
DilmyPerera <samadiniperera20@gmail.com>
2024-04-27 15:03:28 +0530

    feat: :sparkles: create index file

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+
+const taskroutes = require('../backend/src/routes/taskroutes.js');
+const userroutes = require('../backend/src/routes/userroutes.js');
+
+const mongooserequiring = require('./src/db/mongoose.js');
 const config = require('../backend/src/config/config.js');
 
 const app=express();
 
 app.use(express.json());
+
+app.use(userroutes);
+app.use(taskroutes);
 
 app.listen(config.port,()=>{
     console.log(`Server is up on port ${config.port}`);
---
+++ backend/src/routes/taskroutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router =express.Router();
+const cntrl = require('../controllers/controllers.js');
+
+module.exports = router;
