8cf0a383c3b7432e7aaa14cfa47d3e1eaa1f7d0e
DilmyPerera <samadiniperera20@gmail.com>
2024-04-28 09:13:26 +0530

    feat: setup user file

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 
-const taskroutes = require('../backend/src/routes/taskroutes.js');
-const userroutes = require('../backend/src/routes/userroutes.js');
+const taskrouter = require('../backend/src/routes/taskroutes.js');
+const userrouter = require('../backend/src/routes/userroutes.js');
 
-const mongooserequiring = require('./src/db/mongoose.js');
+const mongooserequiring = require('./src/db/db.js');
 const config = require('../backend/src/config/config.js');
 
 const app=express();
 
+const port = process.env.PORT;
+
+
 app.use(express.json());
 
-app.use(userroutes);
-app.use(taskroutes);
+app.use(userrouter);
+app.use(taskrouter);
 
 app.listen(config.port,()=>{
     console.log(`Server is up on port ${config.port}`);
 
 });
+
